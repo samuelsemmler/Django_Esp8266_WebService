b35d39608d916bb4bc1f32536dba0871cba759c0
samuelsemmler <samuelsemmler@hotmail.com>
2020-12-10 15:27:54 -0300

    Esp Api app created, make migrations

---
+++ esp_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EspApiConfig(AppConfig):
+    name = 'esp_api'
--- esp_api/urls.py
+++ esp_api/urls.py
@@ -1,4 +1,4 @@
-"""Django_Esp8266_WebService URL Configuration
+"""esp_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('esp_api.urls'))
+    path('', index),
 ]
